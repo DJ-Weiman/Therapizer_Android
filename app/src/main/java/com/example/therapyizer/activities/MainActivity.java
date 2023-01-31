8436db88e7ecb9bc5098203f69cbe2bb7e4e74e6
DIONS-LEGION\Dionw <dionweiman1998@gmail.com>
2023-01-31 12:18:05 +0530

    Added Sign out functinality

--- app/src/main/java/com/example/therapyizer/activities/MainActivity.java
+++ app/src/main/java/com/example/therapyizer/activities/MainActivity.java
@@ -1,10 +1,18 @@
 package com.example.therapyizer.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.therapyizer.R;
+import com.example.therapyizer.utilities.Constants;
+import com.example.therapyizer.utilities.PreferenceManager;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
@@ -16,20 +24,78 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.therapyizer.databinding.ActivityMainBinding;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.iid.FirebaseInstanceIdReceiver;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
+    private PreferenceManager preferenceManager;
+    ActivityMainBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        preferenceManager = new PreferenceManager(getApplicationContext());
+
+        binding.textTitle.setText(String.format(
+                "%s %s",
+                preferenceManager.getString(Constants.KEY_FIRST_NAME),
+                preferenceManager.getString(Constants.KEY_LAST_NAME)
+        ));
+
+        binding.textSignOut.setOnClickListener(view -> signOut());
+
+        FirebaseMessaging.getInstance().getToken().addOnCompleteListener(task -> {
+            if(task.isSuccessful() && task.getResult() != null){
+                sendFCMTokenToDatabase(task.getResult());
+            }
+        });
+    }
+
+    private void sendFCMTokenToDatabase(String token){
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        DocumentReference documentReference =
+                database.collection(Constants.KEY_COLLECTION_USERS).document(
+                        preferenceManager.getString(Constants.KEY_USER_ID)
+                );
+        documentReference.update(Constants.KEY_FCM_TOKEN, token)
+                .addOnSuccessListener(unused -> Toast.makeText(MainActivity.this, "Token Updated successfully", Toast.LENGTH_SHORT).show())
+                .addOnFailureListener(e -> Toast.makeText(MainActivity.this, "Unable to send Token: " + e.getMessage(), Toast.LENGTH_SHORT).show());
+
+    }
+
+    private void signOut() {
+        Toast.makeText(this, "Signing out...", Toast.LENGTH_SHORT).show();
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+
+        DocumentReference documentReference =
+                database.collection(Constants.KEY_COLLECTION_USERS).document(
+                        preferenceManager.getString(Constants.KEY_USER_ID)
+                );
+
+        HashMap<String, Object> updates = new HashMap<>();
+        updates.put(Constants.KEY_FCM_TOKEN, FieldValue.delete());
+        documentReference.update(updates)
+                .addOnSuccessListener(unused -> {
+                    preferenceManager.clearPreferences();
+                    startActivity(new Intent(getApplicationContext(), SignInActivity.class));
+                    finish();
+                })
+                .addOnFailureListener(e -> Toast.makeText(MainActivity.this, "There was an issue Signing out", Toast.LENGTH_SHORT).show());
     }
 
 }
--- app/src/main/java/com/example/therapyizer/activities/SignInActivity.java
+++ app/src/main/java/com/example/therapyizer/activities/SignInActivity.java
@@ -6,32 +6,94 @@ import androidx.databinding.DataBindingUtil;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
 import android.widget.Toast;
 
 import com.example.therapyizer.R;
 import com.example.therapyizer.databinding.ActivitySignInBinding;
+import com.example.therapyizer.utilities.Constants;
+import com.example.therapyizer.utilities.PreferenceManager;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.HashMap;
 
 public class SignInActivity extends AppCompatActivity {
 
     ActivitySignInBinding binding;
+    private PreferenceManager preferenceManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        preferenceManager = new PreferenceManager(getApplicationContext());
+
+        if(preferenceManager.getBoolean(Constants.KEY_IS_SIGNED_IN)){
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
         binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_in);
 
         binding.signUpText.setOnClickListener(view -> startActivity(new Intent(getApplicationContext(), SignUpActivity.class)));
 
-        FirebaseFirestore database = FirebaseFirestore.getInstance();
-
+        binding.signInButton.setOnClickListener(view -> signInButtonPressed());
     }
 
+    private void signInButtonPressed() {
 
+        if (binding.inputEmail.getText().toString().trim().isEmpty())
+            Toast.makeText(this, "Please Enter Your Email", Toast.LENGTH_SHORT).show();
+        else if (!Patterns.EMAIL_ADDRESS.matcher(binding.inputEmail.getText().toString()).matches())
+            Toast.makeText(this, "Enter a valid email address", Toast.LENGTH_SHORT).show();
+        else if (binding.passwordInput.getText().toString().trim().isEmpty())
+            Toast.makeText(this, "Enter Your password", Toast.LENGTH_SHORT).show();
+        else
+            signIn();
+    }
+
+    private void signIn() {
+
+        binding.signInButton.setVisibility(View.INVISIBLE);
+        binding.signInProgressBar.setVisibility(View.VISIBLE);
+
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        database.collection(Constants.KEY_COLLECTION_USERS)
+                .whereEqualTo(Constants.KEY_EMAIL, binding.inputEmail.getText().toString())
+                .whereEqualTo(Constants.KEY_PASSWORD, binding.passwordInput.getText().toString())
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                        if (task.isSuccessful() && task.getResult() != null && task.getResult().getDocuments().size() > 0) {
+                            DocumentSnapshot documentSnapshot = task.getResult().getDocuments().get(0);
+                            preferenceManager.putBoolean(Constants.KEY_IS_SIGNED_IN, true);
+                            preferenceManager.putString(Constants.KEY_USER_ID, documentSnapshot.getId());
+                            preferenceManager.putString(Constants.KEY_FIRST_NAME, documentSnapshot.getString(Constants.KEY_FIRST_NAME));
+                            preferenceManager.putString(Constants.KEY_LAST_NAME, documentSnapshot.getString(Constants.KEY_LAST_NAME));
+                            preferenceManager.putString(Constants.KEY_EMAIL, documentSnapshot.getString(Constants.KEY_EMAIL));
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(intent);
+                        } else {
+                            binding.signInProgressBar.setVisibility(View.INVISIBLE);
+                            binding.signInButton.setVisibility(View.VISIBLE);
+                            Toast.makeText(SignInActivity.this, "Unable to Sign In", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+
+    }
 }
+
