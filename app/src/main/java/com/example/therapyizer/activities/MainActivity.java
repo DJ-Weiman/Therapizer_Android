447d397ef5ace6a9adc92a31be1abf604e996d45
DIONS-LEGION\Dionw <dionweiman1998@gmail.com>
2023-02-02 14:07:12 +0530

    Added functionality to display users

--- app/src/main/java/com/example/therapyizer/activities/MainActivity.java
+++ app/src/main/java/com/example/therapyizer/activities/MainActivity.java
@@ -2,45 +2,36 @@ package com.example.therapyizer.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.navigation.ui.AppBarConfiguration;
 
 import com.example.therapyizer.R;
+import com.example.therapyizer.adapters.UsersAdapter;
+import com.example.therapyizer.databinding.ActivityMainBinding;
+import com.example.therapyizer.models.User;
 import com.example.therapyizer.utilities.Constants;
 import com.example.therapyizer.utilities.PreferenceManager;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.View;
-
-import androidx.databinding.DataBindingUtil;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.example.therapyizer.databinding.ActivityMainBinding;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.iid.FirebaseInstanceIdReceiver;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.messaging.FirebaseMessaging;
 
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
-
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private PreferenceManager preferenceManager;
     ActivityMainBinding binding;
+    private List<User> users;
+    private UsersAdapter usersAdapter;
 
 
     @Override
@@ -60,20 +51,59 @@ public class MainActivity extends AppCompatActivity {
         binding.textSignOut.setOnClickListener(view -> signOut());
 
         FirebaseMessaging.getInstance().getToken().addOnCompleteListener(task -> {
-            if(task.isSuccessful() && task.getResult() != null){
+            if (task.isSuccessful() && task.getResult() != null) {
                 sendFCMTokenToDatabase(task.getResult());
             }
         });
+
+        users = new ArrayList<>();
+        usersAdapter = new UsersAdapter(users);
+        binding.usersRecyclerView.setAdapter(usersAdapter);
+
+        getUsers();
+
     }
 
-    private void sendFCMTokenToDatabase(String token){
+    private void getUsers() {
+        binding.usersProgressBar.setVisibility(View.VISIBLE);
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        database.collection(Constants.KEY_COLLECTION_USERS)
+                .get()
+                .addOnCompleteListener(task -> {
+                    binding.usersProgressBar.setVisibility(View.GONE);
+                    String myUserId = preferenceManager.getString(Constants.KEY_USER_ID);
+                    if (task.isSuccessful() && task.getResult() != null) {
+                        for (QueryDocumentSnapshot documentSnapshot : task.getResult()) {
+                            if (myUserId.equals(documentSnapshot.getId())) {
+                                continue;
+                            }
+                            User user = new User();
+                            user.firstName = documentSnapshot.getString(Constants.KEY_FIRST_NAME);
+                            user.lastName = documentSnapshot.getString(Constants.KEY_LAST_NAME);
+                            user.email = documentSnapshot.getString(Constants.KEY_EMAIL);
+                            user.token = documentSnapshot.getString(Constants.KEY_FCM_TOKEN);
+                            users.add(user);
+                        }
+                        if (users.size() > 0) {
+                            usersAdapter.notifyDataSetChanged();
+                        } else {
+                            binding.textErrorMessage.setText(String.format("%s", "No Users Available"));
+                            binding.textErrorMessage.setVisibility(View.VISIBLE);
+                        }
+                    } else {
+                        binding.textErrorMessage.setText(String.format("%s", "No Users Available"));
+                        binding.textErrorMessage.setVisibility(View.VISIBLE);
+                    }
+                });
+    }
+
+    private void sendFCMTokenToDatabase(String token) {
         FirebaseFirestore database = FirebaseFirestore.getInstance();
         DocumentReference documentReference =
                 database.collection(Constants.KEY_COLLECTION_USERS).document(
                         preferenceManager.getString(Constants.KEY_USER_ID)
                 );
         documentReference.update(Constants.KEY_FCM_TOKEN, token)
-                .addOnSuccessListener(unused -> Toast.makeText(MainActivity.this, "Token Updated successfully", Toast.LENGTH_SHORT).show())
                 .addOnFailureListener(e -> Toast.makeText(MainActivity.this, "Unable to send Token: " + e.getMessage(), Toast.LENGTH_SHORT).show());
 
     }
