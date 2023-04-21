7966a52c7b49c7114b7f8e69aa720c66e4be638d
DIONS-LEGION\Dionw <dionweiman1998@gmail.com>
2023-04-21 16:39:25 +0530

    Added UI for Patient Therapy Activity

---
+++ app/src/main/java/com/example/therapyizer/activities/patient/TherapyActivity.java
@@ -0,0 +1,13 @@
+package com.example.therapyizer.activities.patient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TherapyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
